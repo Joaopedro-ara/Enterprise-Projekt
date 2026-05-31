e90fd49b17bc41633ef5228c27476444857391a5
Joao <araujo@uni-hildesheim.de>
2026-05-31 20:06:23 +0200

    -> Wir haben den CSRF (Cross-Site Request Forgery)  importiert und  unter der app=flask(name)  die funktion hinzugefügt: csrf=CSRFProtect(app) das bedeutet es schützt den post wenn jemand eingelogt ist und jemand anders geht drauf das nicht dazu gehört dann blockiert Flask ab sofort jede einzelne POST-Anfrage

--- Mitarbeiter/app2.py
+++ Mitarbeiter/app2.py
@@ -2,10 +2,11 @@ from flask import Flask,render_template,request,redirect,url_for,session,send_fi
 from titanflow_enterprise.Mitarbeiter.use_case import usermanger
 from titanflow_enterprise.Mitarbeiter.Lager_manager import Lagermanger
 from titanflow_enterprise.Mitarbeiter.routes_produktion import produktion_bp
-
-
+from flask_wtf.csrf import CSRFProtect # CSRF (Cross-Site Request Forgery) # vor externen eingriffe schützen
+#also es blockiert jede Post Anfrage die an den routen geschickt werden
 
 app=Flask(__name__)
+csrf=CSRFProtect(app)
 app.secret_key="Hilde-super-geheim-2026"
 app.register_blueprint(produktion_bp)
 manger=usermanger()
